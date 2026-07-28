12f50a9f2a032639baf080506037631a655b41e2
SURAJ PAWAR <121868743+Suraj7698@users.noreply.github.com>
2026-07-28 16:58:39 +0530

    Update skills.tsx

--- artifacts/portfolio/src/components/sections/skills.tsx
+++ artifacts/portfolio/src/components/sections/skills.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
-import { SiReact, SiNodedotjs, SiMongodb, SiTypescript, SiTailwindcss, SiExpress, SiGit, SiHtml5, SiJavascript, SiGithub, SiFigma } from "react-icons/si";
+import { SiReact, SiNodedotjs, SiMongodb, SiTypescript, SiTailwindcss, SiExpress, SiGit, SiHtml5, SiJavascript, SiGithub, SiFigmaSiVisualstudiocode,
+  SiVercel} from "react-icons/si";
 
 const skillCategories = [
   {
